174daf46cd5d20226160805e82e2139ec3dc33b3
LukeM17 <luke@ordr.com.au>
2021-02-03 20:20:29 +1100

    Finished version

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// Inspirational messages
-let messages= ['Success is a lousy teacher. It seduces smart people into thinking they can\'t- Bill Gates', 
-'Your time is limited, so don\'t waste it living someone else\'s life- Steve Jobs','Be humble in victory or defeat- Conor McGregor', 'I like criticism. It makes me stronger- LeBron James',
- ];
 
-// number of the day.
-//const numb=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+const Randomize = () => { 
+// messages and numbers
+let messages = ['Success is a lousy teacher. It seduces smart people into thinking they can\'t- Bill Gates', 
+'Your time is limited, so don\'t waste it living someone else\'s life- Steve Jobs', 
+'Be humble in victory or defeat- Conor McGregor', 'I like criticism. It makes me stronger- LeBron James', ]; 
+let numbers = [1,2,3,4,5,6,7,8,9];
 
-// randomizing the messages from messages array
- const randomGen = () =>{
-    for(let i = 0; i < messages.length; i++){
-    return messages[Math.floor(Math.random()* messages.length)]
-   };
- 
- };
+// make my message and numbers random.
+let randomMsg = Math.floor(Math.random() * messages.length); 
+let randomNum = Math.floor(Math.random() * numbers.length);
 
-randomGen();
+//outputs the random end result to the console 
+let output = []; output.push('Your motivational quote is: ' + messages[randomMsg] + ' and your number is: ' + numbers[randomNum]); 
+return output };
 
-console.log(randomGen())
+console.log(Randomize());
